f9d8eeccc9f06b01e15fb7bf935c2f08c789e850
qqq1018515687 <qq67004014@gmail.com>
2026-07-02 11:23:29 +0800

    fix: disable seat_maps migration to resolve multiple heads

--- migrations/versions/h1i2j3k4l5m6_add_deleted_image_urls.py
+++ migrations/versions/h1i2j3k4l5m6_add_deleted_image_urls.py
@@ -1,25 +1,23 @@
-"""add deleted_image_urls column to tasks table - merge heads
+"""add deleted_image_urls column to tasks table
 
 Revision ID: h1i2j3k4l5m6
-Revises: v001_add_seat_maps
+Revises: 0d1e2f3a4b5c
 Create Date: 2026-07-02 00:00:00.000000
 
-This migration also depends on 0d1e2f3a4b5c to merge the migration branches.
 """
-from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
 
-revision: str = "h1i2j3k4l5m6"
-down_revision: Union[str, Sequence[str], None] = ("0d1e2f3a4b5c", "v001_add_seat_maps")
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision = "h1i2j3k4l5m6"
+down_revision = "0d1e2f3a4b5c"
+branch_labels = None
+depends_on = None
 
 
-def upgrade() -> None:
+def upgrade():
     op.add_column('tasks', sa.Column('deleted_image_urls', sa.JSON(), nullable=True))
 
 
-def downgrade() -> None:
+def downgrade():
     op.drop_column('tasks', 'deleted_image_urls')
